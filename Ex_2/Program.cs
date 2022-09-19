40bc85bf6adb8bbca9441636009f7ff3ea199e05
Darya <darya.ogurtsova.99@mail.ru>
2022-09-20 00:33:55 +0300

    Добавлено решение

---
+++ Ex_2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+ {
+ 
+ string[] arr = { "hello", "2", "world", ":-)" };
+ int kolRazmItv = 0;
+ int curind = 0;
+ 
+ for (int i = 0; i < arr.Length; i++) {
+ if ( arr[i].Length <= 3 ) {
+ kolRazmItv+=1;
+ }
+ }
+ 
+ string[] otv = new string[kolRazmItv]; 
+ for (int i = 0; i < arr.Length; i++) {
+ if ( arr[i].Length <= 3 ) {
+ otv[curind]= arr[i];
+ curind+=1;
+ }
+ }
+ 
+ for (int i = 0; i < otv.Length; i++) {
+ Console.WriteLine(otv[i]);
+ } 
+
+ }
